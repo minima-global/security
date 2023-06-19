394c4c540aa86246c9fdfa22326846b1fea87813
Elias <eliasnemr@gmail.com>
2023-06-19 16:20:13 +0300

    change logs
    - getAppUID
    - skip splash if not first time

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
 import AppProvider from "./AppContext";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { AuthProvider } from "./providers/authProvider";
+import { useEffect } from "react";
+import * as utils from "./utils";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const ls = localStorage.getItem(utils.getAppUID());
+
+    if (!ls) {
+      return localStorage.setItem(utils.getAppUID(), "1");
+    }
+
+    if (ls) {
+      navigate("/dashboard");
+    }
+  }, []);
+
   return (
     <AppProvider>
       <AuthProvider>
